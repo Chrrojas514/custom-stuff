20ff23ac0d25cb6041b592a2c0ab31df50e8459d
unknown <chrisrojas514@gmail.com>
2023-07-05 18:18:58 -0400

    Added lamp, a custom item. Still missing assets

--- src/main/java/com/cillow/customstuff/CustomStuff.java
+++ src/main/java/com/cillow/customstuff/CustomStuff.java
@@ -1,15 +1,19 @@
 package com.cillow.customstuff;
 
+import com.cillow.customstuff.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class CustomStuff implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("custom-stuff");
+
+	public static final String MOD_ID = "custom-stuff";
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +22,8 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/com/cillow/customstuff/CustomStuffClient.java
@@ -0,0 +1,10 @@
+package com.cillow.customstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
